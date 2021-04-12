b275c4d054e4c9b9dc2e5d60c96fd8efdbbe9faf
Mukul Mehta <mukul.csiitkgp@gmail.com>
2021-04-12 23:07:41 +0530

    Add Subtask B code and results

--- subtask_B/utils.py
+++ subtask_B/utils.py
@@ -1,32 +1,34 @@
 import torch
 
-## Utility functions for training model
 
 def create_input(seq, tokenizer):
-    for count,i in enumerate(seq):
-       temp = tokenizer.tokenize(i) 
-       if(len(temp)>1): 
-         seq[count] = temp[0]           
+    for count, i in enumerate(seq):
+        temp = tokenizer.tokenize(i)
+        if(len(temp) > 1):
+            seq[count] = temp[0]
     sentences = " ".join(seq)
     inputs = tokenizer(sentences, return_tensors="pt")
     return inputs
+
 
 def log_func(vec):
     max_score = vec[0, maxval(vec)]
     max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
     return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
 
+
 def maxval(vec):
     _, idx = torch.max(vec, 1)
     return idx.item()
 
-def BILOU_substring(n): 
-  if n==0:
-    return ''
-  elif n ==1:
-    return 'U'
-  elif n==2:
-    return 'B L'
-  else:
-    t1 = 'I '*(n-2)
-    return 'B '+t1+'L'
+
+def BILOU_substring(n):
+    if n == 0:
+        return ''
+    elif n == 1:
+        return 'U'
+    elif n == 2:
+        return 'B L'
+    else:
+        t1 = 'I '*(n-2)
+        return 'B '+t1+'L'
